dbb4b5eb7a03871b5603d677f39991e041f6ef72
gdelr <32882550+gdelr@users.noreply.github.com>
2022-04-21 00:35:49 -0400

    testing on my windowspc

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ for (const file of commandFiles) {
 client.once('ready', () => {
 	console.log('Ready!');
 });
-
+// test change 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
